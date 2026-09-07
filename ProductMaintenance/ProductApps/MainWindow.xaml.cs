04e12d0c8598c1f50a042533ef9c159f3adef98e
Jaxon Mossop <jaxon.mossop@student.tafesa.edu.au>
2026-09-07 12:44:18 +0930

    Added GST calc

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 paymentAfterFeeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 paymentAfterWrapFeeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                paymentAfterGSTTextBlock.Text = Convert.ToString((double) (cProduct.TotalPayment + 25 + 5) * 1.1);
             }
             catch (FormatException)
             {
